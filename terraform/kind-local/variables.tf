e76f6c6034819f2a26cbe0b5621b6a4239c43d74
Carl Luo <4597409+mdluo@users.noreply.github.com>
2025-07-24 10:32:06 +0800

    upgrade cilium, gateway api, argocd, gitea, reflector versions

--- terraform/kind-local/variables.tf
+++ terraform/kind-local/variables.tf
@@ -17,13 +17,13 @@ variable "kubernetes_version" {
 # https://artifacthub.io/packages/helm/cilium/cilium
 variable "cilium_version" {
   type    = string
-  default = "1.17.6"
+  default = "1.18.0-rc.1"
 }
 
 # https://docs.cilium.io/en/stable/network/servicemesh/gateway-api/gateway-api/#prerequisites
 variable "gateway_api_version" {
   type    = string
-  default = "v1.2.0"
+  default = "v1.3.0"
 }
 
 # https://artifacthub.io/packages/helm/traefik/traefik
@@ -35,7 +35,7 @@ variable "traefik_helm_version" {
 # https://artifacthub.io/packages/helm/argo/argo-cd
 variable "argocd_helm_version" {
   type    = string
-  default = "8.1.3"
+  default = "8.2.0"
 }
 
 # https://argo-cd.readthedocs.io/en/stable/user-guide/directory/#including-only-certain-file
