bb97f566f53905dbb2f8f99d6553bd2d2ed85de6
Carl Luo <4597409+mdluo@users.noreply.github.com>
2025-07-29 15:35:08 +0800

    platform: seed script

---
+++ packages/platform/scripts/db-seed.ts
@@ -0,0 +1,18 @@
+import { sql } from 'drizzle-orm';
+
+import { environmentTable } from '#drizzle/environment.ts';
+import { orgTable } from '#drizzle/org.ts';
+import { orgDomainTable } from '#drizzle/org_domain.ts';
+import { db } from '#server/lib/db.ts';
+
+await db.transaction(async (tx) => {
+  await tx.execute(sql`truncate table org restart identity cascade`);
+  await tx.execute(sql`truncate table org_domain restart identity cascade`);
+  await tx.execute(sql`truncate table environment restart identity cascade`);
+
+  await tx.insert(orgTable).values({ id: 'voltade', display_name: 'Voltade' });
+  await tx
+    .insert(orgDomainTable)
+    .values({ org_id: 'voltade', domain: 'voltade.com' });
+  await tx.insert(environmentTable).values({ org_id: 'voltade' });
+});
