19f46e375febca4e4bf1a8d4af2ece65847629a4
yeojunjie <54196980+yeojunjie@users.noreply.github.com>
2025-07-30 12:31:26 +0800

    feat: backend filtering for MRT in app-template

--- packages/app-template/src/components/ProductTemplatesTable/ProductTemplatesMRT.tsx
+++ packages/app-template/src/components/ProductTemplatesTable/ProductTemplatesMRT.tsx
@@ -8,6 +8,9 @@ import {
 import {
   MantineReactTable,
   type MRT_ColumnDef,
+  type MRT_ColumnFilterFnsState,
+  type MRT_ColumnFiltersState,
+  type MRT_PaginationState,
   useMantineReactTable,
 } from 'mantine-react-table';
 import { useEffect, useState } from 'react';
@@ -21,37 +24,57 @@ const columns: MRT_ColumnDef<ProductTemplate>[] = [
   {
     accessorKey: 'id',
     header: 'ID',
+    filterFn: 'equals',
   },
   {
     accessorKey: 'name',
     header: 'Name',
+    filterFn: 'contains',
   },
   {
     accessorKey: 'description',
     header: 'Description',
+    filterFn: 'contains',
   },
   {
     accessorKey: 'list_price',
     header: 'List Price',
+    filterFn: 'equals',
   },
   {
+    // TODO: Improve UX for category filtering.
+    // Currently, entering a non-existing category will cause a database-level error.
     accessorKey: 'category',
     header: 'Category',
+    filterFn: 'equals',
   },
 ];
 
 export default function ProductTemplatesMRT() {
-  const [pagination, setPagination] = useState({
+  const [pagination, setPagination] = useState<MRT_PaginationState>({
     pageIndex: 0,
     pageSize: 10,
   });
 
   const [rowCount, setRowCount] = useState(0);
 
+  const [columnFilters, setColumnFilters] = useState<MRT_ColumnFiltersState>(
+    [],
+  );
+
+  const [columnFilterFns, setColumnFilterFns] =
+    useState<MRT_ColumnFilterFnsState>(
+      Object.fromEntries(
+        columns.map(({ accessorKey, filterFn }) => [accessorKey, filterFn]),
+      ) as MRT_ColumnFilterFnsState,
+    );
+
   const { data, isLoading } = useProductTemplates({
     page: pagination.pageIndex + 1,
     pageSize: pagination.pageSize,
     supabase,
+    columnFilters,
+    columnFilterFns,
   });
 
   useEffect(() => {
@@ -64,12 +87,17 @@ export default function ProductTemplatesMRT() {
     columns,
     data: data?.data ?? [],
     onPaginationChange: setPagination,
-    state: { pagination, isLoading },
+    state: { pagination, isLoading, columnFilterFns, columnFilters },
     manualPagination: true,
     enablePagination: true,
     enableSorting: false, // TODO: Backend sorting.
-    enableFilters: false, // TODO: Backend filtering.
     rowCount,
+    manualFiltering: true,
+    enableFilterMatchHighlighting: false,
+    enableColumnFilters: true,
+    enableGlobalFilter: false,
+    onColumnFilterFnsChange: setColumnFilterFns,
+    onColumnFiltersChange: setColumnFilters,
   });
 
   return (
--- packages/core-schema/supabase/hooks/products/useProductTemplates.ts
+++ packages/core-schema/supabase/hooks/products/useProductTemplates.ts
@@ -1,4 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
+import type {
+  MRT_ColumnFilterFnsState,
+  MRT_ColumnFiltersState,
+} from 'mantine-react-table';
 
 import type { Database, SupabaseClient } from '../../../supabase/index.ts';
 
@@ -9,6 +13,8 @@ interface UseProductTemplatesOptions {
   page: number;
   pageSize: number;
   supabase: SupabaseClient;
+  columnFilters: MRT_ColumnFiltersState;
+  columnFilterFns: MRT_ColumnFilterFnsState;
 }
 
 interface UseProductTemplatesResult {
@@ -20,17 +26,38 @@ export function useProductTemplates({
   page,
   pageSize,
   supabase,
+  columnFilters,
+  columnFilterFns,
 }: UseProductTemplatesOptions) {
   return useQuery({
-    queryKey: ['product_template', { page, limit: pageSize }],
+    queryKey: [
+      'product_template',
+      { page, limit: pageSize, columnFilters, columnFilterFns },
+    ],
     queryFn: async (): Promise<UseProductTemplatesResult> => {
       const from = (page - 1) * pageSize;
       const to = from + pageSize - 1;
 
-      const { data, error, count } = await supabase
+      const query = supabase
         .from('product_template_view')
-        .select('*', { count: 'exact' })
-        .range(from, to);
+        .select('*', { count: 'exact' });
+
+      for (const columnId in columnFilterFns) {
+        // TODO: Support other filter functions.
+        if (columnFilterFns[columnId] === 'contains') {
+          const filter = columnFilters.find((f) => f.id === columnId);
+          if (filter) {
+            query.ilike(columnId, `%${filter.value}%`);
+          }
+        } else if (columnFilterFns[columnId] === 'equals') {
+          const filter = columnFilters.find((f) => f.id === columnId);
+          if (filter) {
+            query.eq(columnId, `${filter.value}`);
+          }
+        }
+      }
+
+      const { data, error, count } = await query.range(from, to);
 
       if (error) {
         throw new Error(`Failed to fetch products: ${error.message}`);
