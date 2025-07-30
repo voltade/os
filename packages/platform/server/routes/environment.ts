a3508739016b095e180c173f4e957f5c0bdba2e6
Carl Luo <4597409+mdluo@users.noreply.github.com>
2025-07-30 17:42:33 +0800

    environment supabase httproute and tls certificate

--- packages/platform/server/routes/environment.ts
+++ packages/platform/server/routes/environment.ts
@@ -38,7 +38,7 @@ export const route = factory
         variables: {
           orgId: environment.org_id,
           environmentId: environment.id,
-          environmentChartVersion: '0.1.16',
+          environmentChartVersion: '0.1.19',
           releaseName: `${environment.org_id}-${environment.id}`,
           isProduction: environment.is_production,
         },
