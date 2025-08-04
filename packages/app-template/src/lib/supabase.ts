81c42d171c34188f75471402151e181133e44f70
RussellArvin <russellarvin.soc@gmail.com>
2025-08-04 20:56:59 +0800

    feat: use jwt token for pgrest authentication

--- packages/app-template/src/lib/supabase.ts
+++ packages/app-template/src/lib/supabase.ts
@@ -1,17 +1,18 @@
 import { createClient } from '@voltade/core-schema/supabase';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseAnonKey = localStorage.getItem('voltade-jwt') || '';
 
 //TODO: fix this later
 if (!supabaseUrl) {
   throw new Error('VITE_SUPABASE_URL is required');
 }
 
-if (!supabaseAnonKey) {
-  throw new Error('VITE_SUPABASE_ANON_KEY is required');
+if (supabaseAnonKey === '') {
+  console.log('Key:', supabaseAnonKey);
+  throw new Error('Voltade JWT is required. Please log in to get a valid JWT.');
 }
 
-export const supabase = createClient(supabaseUrl + '/rest/v1', supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export type SupabaseClient = typeof supabase;
