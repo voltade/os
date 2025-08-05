047a5948fa550becadb1729985d2203c74c0e70e
yeojunjie <54196980+yeojunjie@users.noreply.github.com>
2025-08-05 15:50:13 +0800

    core-schema: set up education schema

---
+++ packages/core-schema/schemas/education/tables/privileges.sql
@@ -0,0 +1,15 @@
+grant usage on schema education to anon,
+authenticated,
+service_role;
+
+grant all on all tables in schema education to anon,
+authenticated,
+service_role;
+
+grant all on all routines in schema education to anon,
+authenticated,
+service_role;
+
+grant all on all sequences in schema education to anon,
+authenticated,
+service_role;
