177cc4e2bce6cb4d46b8f2f786b54f3267b36592
Carl Luo <4597409+mdluo@users.noreply.github.com>
2025-08-15 16:02:42 +0800

    cli: build and upload app

--- packages/voltade-cli/src/commands/app/install.ts
+++ packages/voltade-cli/src/commands/app/install.ts
@@ -1,10 +1,11 @@
-import fs from 'node:fs/promises';
+import fs from 'node:fs';
 import path from 'node:path';
 import { $ } from 'bun';
 import { select } from '@inquirer/prompts';
-import { Args } from '@oclif/core';
+import { Args, Flags } from '@oclif/core';
 import { titleCase } from 'scule';
 import slug from 'slug';
+import * as tar from 'tar';
 import z from 'zod';
 
 import { BaseCommand } from '#src/base.ts';
@@ -23,9 +24,18 @@ export default class Install extends BaseCommand {
   static override args = {
     appPath: Args.string(),
   };
+  static override flags = {
+    ...BaseCommand.flags,
+    'skip-build': Flags.boolean({
+      description: 'Skip the build step and only install the app',
+      default: false,
+    }),
+  };
 
   public async run(): Promise<void> {
     const { spinner, honoClient, authClient } = this;
+    const { args, flags } = await this.parse(Install);
+
     console.log('Installing app...');
 
     spinner.start('Fetching organizations...');
@@ -67,14 +77,13 @@ export default class Install extends BaseCommand {
         })),
     });
 
-    const { args } = await this.parse(Install);
     const appFullPath = args.appPath
       ? path.resolve(args.appPath)
       : process.cwd();
 
     const parseRes = packageJsonSchema.safeParse(
       JSON.parse(
-        await fs.readFile(path.join(appFullPath, 'package.json'), 'utf-8'),
+        fs.readFileSync(path.join(appFullPath, 'package.json'), 'utf-8'),
       ),
     );
     if (!parseRes.success) {
@@ -89,7 +98,7 @@ export default class Install extends BaseCommand {
     spinner.stop();
     const apps = await appsRes.json();
 
-    const appSlug = slug(pkgJson.name);
+    const appSlug = slug(pkgJson.name.replace('/', '-'));
     let app = apps.find((a) => a.slug === appSlug);
     if (!app) {
       spinner.warn(`App ${appSlug} not found in this organization.`);
@@ -101,6 +110,7 @@ export default class Install extends BaseCommand {
           description: pkgJson.description,
           git_repo_url: 'http://example.com/repo.git',
           build_command: `bun run build`,
+          output_path: 'dist',
         },
       });
       spinner.stop();
@@ -111,12 +121,86 @@ export default class Install extends BaseCommand {
       app = await createAppRes.json();
     }
 
-    spinner.start(`Building app ${appSlug}...`);
-    const buildCommand = app.build_command ?? 'bun run build';
+    if (flags['skip-build']) {
+      this.log(`Skipping build for app ${appSlug}.`);
+    } else {
+      this.log(`Building app ${appSlug}...`);
+      const buildCommand = app.build_command ?? 'bun run build';
+      $.cwd(appFullPath);
+      await $`${{ raw: buildCommand }}`;
+    }
 
-    $.cwd(appFullPath);
-    await $`${{ raw: buildCommand }}`;
+    spinner.start(`Creating app build...`);
+    const appBuildRes = await honoClient.app_build.s3.signed_url.$post({
+      json: {
+        orgId,
+        appId: app.id,
+      },
+    });
+    if (!appBuildRes.ok) {
+      const error = await appBuildRes.text();
+      this.error(`Failed to create app build: ${error}`);
+    }
+    const { appBuild, uploadUrl } = await appBuildRes.json();
+    spinner.stop();
 
+    const outputPath = path.resolve(appFullPath, app.output_path);
+    if (!fs.existsSync(outputPath)) {
+      this.error(`Output path ${outputPath} does not exist.`);
+    }
+    const tmpDir = process.env.TMPDIR || '/tmp';
+    const tarFilePath = path.join(tmpDir, `${appBuild.id}.tar.gz`);
+
+    spinner.start(`Archiving app build...`);
+    await tar.create(
+      {
+        gzip: true,
+        cwd: outputPath,
+        file: tarFilePath,
+        portable: true,
+      },
+      ['.'],
+    );
+    spinner.stop();
+    this.log(`App build archived to ${tarFilePath}`);
+    const tarFile = Bun.file(tarFilePath);
+
+    spinner.start(`Uploading app build...`);
+    const uploadRes = await fetch(uploadUrl, {
+      method: 'PUT',
+      body: tarFile,
+    });
+    if (!uploadRes.ok) {
+      const error = await uploadRes.text();
+      this.error(`Failed to upload app build: ${error}`);
+    }
+    spinner.stop();
+    this.log(`App build uploaded successfully.`);
+
+    await honoClient.app_build[':buildId'].status.$patch({
+      param: {
+        buildId: appBuild.id,
+      },
+      json: {
+        orgId,
+        appId: app.id,
+        status: 'ready',
+      },
+    });
+
+    spinner.start(`Installing app build...`);
+    const installRes = await honoClient.app_installation.$post({
+      json: {
+        organization_id: orgId,
+        environment_id: envId,
+        app_id: app.id,
+        app_build_id: appBuild.id,
+      },
+    });
+    if (!installRes.ok) {
+      const error = await installRes.text();
+      this.error(`Failed to install app: ${error}`);
+    }
     spinner.stop();
   }
 }
