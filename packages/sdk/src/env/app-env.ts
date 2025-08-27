3cc6c01a7e518cb28367f5919e7a35dab9dde66a
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2025-08-27 17:31:03 +0800

    changes to common envs

--- packages/sdk/src/env/app-env.ts
+++ packages/sdk/src/env/app-env.ts
@@ -3,18 +3,26 @@ import { z } from 'zod';
 // Define the schema
 export const appEnvVariablesSchema = z.object({
   VITE_NODE_ENV: z.string().default('production'),
-  VITE_PLATFORM_URL: z.url(),
+
+  // PGREST
   VITE_PGREST_URL: z.url(),
 
+  // Platform
+  PLATFORM_URL: z.url(),
+  VITE_PLATFORM_URL: z.url(),
+
+  // Organization & Environment
+  ORGANIZATION_ID: z.string(),
+  ORGANIZATION_SLUG: z.string(),
+  ENVIRONMENT_ID: z.string(),
+  ENVIRONMENT_SLUG: z.string(),
+
+  // Database
   DB_USER: z.string(),
   DB_PASSWORD: z.string(),
   DB_HOST: z.string(),
   DB_PORT: z.coerce.number(),
   DB_NAME: z.string(),
-
-  PLATFORM_URL: z.string(),
-  ORGANIZATION_ID: z.string(),
-  RUNNER_SECRET_TOKEN: z.string(),
 });
 
 export const env = appEnvVariablesSchema.parse(process.env);
