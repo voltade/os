aad803fed5a2f6ec120d6883c97f6d456266e0e8
Kaung Nyan Lin <darkliyznyanlin910@gmail.com>
2025-08-27 16:48:50 +0800

    fix apps #2

--- apps/core/app-template/server/env.ts
+++ apps/core/app-template/server/env.ts
@@ -1,3 +1,7 @@
-import type { AppEnvVariables } from '#server/zod/env.ts';
+import {
+  type AppEnvVariables,
+  appEnvVariablesSchema,
+} from '#server/zod/env.ts';
 
+export const appEnvVariables = appEnvVariablesSchema.parse(process.env);
 export type Variables = Record<string, unknown> & AppEnvVariables;
--- apps/education/student-management/server/env.ts
+++ apps/education/student-management/server/env.ts
@@ -1,3 +1,7 @@
-import { appEnvVariablesSchema } from '#server/zod/env.ts';
+import {
+  type AppEnvVariables,
+  appEnvVariablesSchema,
+} from '#server/zod/env.ts';
 
 export const appEnvVariables = appEnvVariablesSchema.parse(process.env);
+export type Variables = Record<string, unknown> & AppEnvVariables;
