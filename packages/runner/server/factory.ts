3bbcb636b8e8328cc2fb3132131abc1844805d5a
RussellArvin <russellarvin.soc@gmail.com>
2025-08-27 16:54:17 +0800

    fix: incorrect referencing for env on runner

--- packages/runner/server/factory.ts
+++ packages/runner/server/factory.ts
@@ -1,7 +1,7 @@
 import { createFactory } from 'hono/factory';
 
-import { appEnvVariables } from '#server/env.ts';
 import type { AppEnvVariables } from '#server/zod/env.ts';
+import { appEnvVariables } from '#server/zod/env.ts';
 import type { Variables } from './env.ts';
 
 export const factory = createFactory<{
