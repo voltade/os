52196c271147e6e95f274b5fd929af7059de2d90
yeojunjie <54196980+yeojunjie@users.noreply.github.com>
2025-09-03 12:54:19 +0800

    core schema: add column education.attendances.journal_line_id

--- packages/core-schema/schemas/education/tables/attendance.ts
+++ packages/core-schema/schemas/education/tables/attendance.ts
@@ -1,6 +1,7 @@
 import { relations } from 'drizzle-orm';
 import { boolean, foreignKey, integer } from 'drizzle-orm/pg-core';
 
+import { journalLineTable } from '../../accounting/tables/journal_line.ts';
 import { DEFAULT_COLUMNS } from '../../utils.ts';
 import { educationAttendanceStatus } from '../enums.ts';
 import { educationSchema } from '../schema.ts';
@@ -19,6 +20,11 @@ export const educationAttendanceTable = educationSchema.table(
       .references(() => educationLessonTable.id),
     rescheduled_to: integer('rescheduled_to'),
     rescheduled_from: integer('rescheduled_from'),
+    // journal_line_id allows an invoice line item to know (directly) which
+    // attendances it is paying for, and (indirectly) which lessons it corresponds to.
+    journal_line_id: integer('journal_line_id').references(
+      () => journalLineTable.id,
+    ),
     is_paid_for: boolean().notNull().default(false),
     is_trial: boolean().notNull(),
     status: educationAttendanceStatus(),
@@ -33,6 +39,11 @@ export const educationAttendanceTable = educationSchema.table(
       columns: [table.rescheduled_from],
       foreignColumns: [table.id],
       name: 'attendance_rescheduled_from_fkey',
+    }),
+    foreignKey({
+      columns: [table.journal_line_id],
+      foreignColumns: [journalLineTable.id],
+      name: 'attendance_journal_line_id_fkey',
     }),
   ],
 );
@@ -57,6 +68,10 @@ export const educationAttendanceTableRelations = relations(
       fields: [educationAttendanceTable.rescheduled_from],
       references: [educationAttendanceTable.id],
       relationName: 'rescheduled_from',
+    }),
+    journalLine: one(journalLineTable, {
+      fields: [educationAttendanceTable.journal_line_id],
+      references: [journalLineTable.id],
     }),
   }),
 );
